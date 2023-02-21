e6f1114c974b7606dfb3059962a2264bef20a206
clever-cr <cleverumuhuza@gmail.com>
2023-02-21 08:57:44 +0200

    Add navigation functionality

--- components/layout/NavBar.jsx
+++ components/layout/NavBar.jsx
@@ -1,14 +1,17 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Image from "next/image";
 import { CiUser, CiSearch } from "react-icons/ci";
 import { AiOutlineMenu } from "react-icons/ai";
 import Link from "next/link";
 const NavBar = () => {
-  const [open, setOpen] = useState(false);
-  const handleOpen = () => {
-    setOpen(!open);
-  };
+  const [menuIsOpen, setMenuIsOpen] = useState(false);
+
+  const handleOpenMenu = () => setMenuIsOpen(!menuIsOpen);
+  useEffect(() => {
+    document.body.style.overflow = menuIsOpen ? "hidden" : "auto";
+    return () => (document.body.style.overflow = "scroll");
+  }, [menuIsOpen]);
   return (
     <div className="px-134 fixed w-full z-50 sm:px-0 bg-light">
       <div className="max-w-[1258px] mx-auto">
@@ -29,10 +32,92 @@ const NavBar = () => {
             <p className="text-xl">EN</p>
             <CiUser className="w-8 h-6" />
             <CiSearch className="w-8 h-6" />
-            {}
-            <AiOutlineMenu className="w-8 h-6" />
+
+            <AiOutlineMenu onClick={handleOpenMenu} className="w-8 h-6" />
           </div>
         </div>
+        {menuIsOpen ? (
+          <section className="fixed inset-0 z-50 grid h-screen mx-auto bg-white max-w-7xl">
+            <div className="overflow-y-scroll">
+              <div
+                onClick={handleOpenMenu}
+                className="bg-[#F2EFED] ml-auto p-5 cursor-pointer absolute top-0 right-0"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className=""
+                  width="30px"
+                  height="30px"
+                  viewBox="0 0 64 64"
+                  fill="none"
+                  stroke="#000000"
+                >
+                  <line x1="16" y1="16" x2="48" y2="48" />
+                  <line x1="48" y1="16" x2="16" y2="48" />
+                </svg>
+              </div>
+              <div className="">
+                <div className="flex flex-col justify-between space-y-10 cursor-pointer pt-52 px-36">
+                  {[
+                    "Study at VU Amsterdam",
+                    "Research",
+                    "About VU Amsterdam",
+                    "VU press office",
+                    "Alumni",
+                    "Student",
+                    "Employee",
+                  ].map((e, i) => {
+                    return (
+                      <Link key={i} href={"/page1"}>
+                        <div className="flex justify-between max-w-sm">
+                          <p className="text-[24px] leading-[30px] whitespace-nowrap font-semibold">
+                            {" "}
+                            {e}
+                          </p>
+                          {i === 0 || i === 1 ? (
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              width="30"
+                              height="30"
+                              viewBox="0 0 24 24"
+                              fill="none"
+                              stroke="currentColor"
+                              stroke-width="1.1"
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                              class="feather feather-plus"
+                            >
+                              <line x1="12" y1="5" x2="12" y2="19" />
+                              <line x1="5" y1="12" x2="19" y2="12" />
+                            </svg>
+                          ) : (
+                            <svg
+                              className="ml-36"
+                              Name="fill-[#000000]"
+                              xmlns="http://www.w3.org/2000/svg"
+                              width="30px"
+                              height="30px"
+                              viewBox="0 0 24 24"
+                              fill="#000000"
+                            >
+                              <path
+                                d="M20 12L4 12M20 12L14 18M20 12L14 6"
+                                stroke="#000000"
+                                stroke-width="1.1"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                              />
+                            </svg>
+                          )}
+                        </div>
+                      </Link>
+                    );
+                  })}
+                </div>
+              </div>
+            </div>
+          </section>
+        ) : null}
       </div>
     </div>
   );
---
+++ pages/page1.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page1 = () => {
+    return (
+        <div><h3>Welcome to Vu</h3></div>
+    )
+}
+
+export default page1
